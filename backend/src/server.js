457310d3cada95fa3f59ba556e3e1f3b49707c44
Marcel Botelho <marcelbotelho1@hotmail.com>
2020-05-24 19:08:58 -0300

    Controle de reserva 22:17

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,18 +3,38 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
 
+const socketio = require('socket.io');
+const http = require('http');
+
 const routes = require('./routes');
+
+const app = express();
+const server = http.Server(app);
+const io = socketio(server);
+
+const connectedUsers = {};
+
+io.on('connection', socket => {
+    const { user_id }  = socket.handshake.query;
+
+    connectedUsers[user_id] = socket.id;
+});
 
 mongoose.connect('mongodb+srv://admin:m4rc3l@cluster0-zfums.mongodb.net/abnb?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-});
+});    
 
-const app = express();
+app.use((req, res, next) => {
+    req.io = io;
+    req.connectedUsers = connectedUsers;
+
+    return next();
+})
 
 app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,11 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import socketio from 'socket.io-client';
 import api from '../../services/api';
 
 import './styles.css';
 
 export default function Dashboard() {
     const [spots, setSpots] = useState([]);
+
+    useEffect(() => {
+        const user_id = localStorage.getItem('user');
+        const socket = socketio('http://192.168.1.102:3333', {
+            query: { user_id },
+        });
+
+        socket.on('booking_request', data => {
+            console.log(data);
+        })
+    }, []);
 
     useEffect(() => {
         async function loadSpots() {
@@ -21,20 +33,20 @@ export default function Dashboard() {
     }, [])
 
     return (
-       <>
-        <ul className="spot-list">
-            {spots.map(spot => (
-                <li key={spot._id}>
-                    <header style={{ backgroundImage: `url(${spot.thumbnail_url})` }} />
-                    <strong>{spot.company}</strong>
-                    <span>{spot.price ? `R${spot.price}/dia` : 'GRATUITO'}</span>
-                </li>
-            ))}
-        </ul>
+        <>
+            <ul className="spot-list">
+                {spots.map(spot => (
+                    <li key={spot._id}>
+                        <header style={{ backgroundImage: `url(${spot.thumbnail_url})` }} />
+                        <strong>{spot.company}</strong>
+                        <span>{spot.price ? `R${spot.price}/dia` : 'GRATUITO'}</span>
+                    </li>
+                ))}
+            </ul>
 
-        <Link to="/new">
-            <button className="btn" >Cadastrar novo spot</button>
-        </Link>
-       </> 
+            <Link to="/new">
+                <button className="btn" >Cadastrar novo spot</button>
+            </Link>
+        </>
     )
 };
